5c2707338d2aea28edc12cc821f3bc51f89370c5
luchodeco <72599025+louisdeco@users.noreply.github.com>
2025-03-12 08:28:28 +0400

    chore: initial project setup with webpack and Jest
    - Configure webpack for development and production environments
    - Set up Jest for testing JavaScript functions
    - Add linting and formatting with ESLint and Prettier
    - Configure Husky and lint-staged for pre-commit hooks
    - Add deployment script for GitHub Pages

---
+++ webpack/webpack.common.js
@@ -0,0 +1,41 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+  entry: "./src/testing-practice.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: "body",
+    }),
+  ],
+};
